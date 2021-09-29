34baea3c27c88e282b1f31b706a79c33dfec1f71
Kevin Barrera <kbarrerac@unal.edu.co>
2021-09-29 00:25:10 -0500

    Router for ecommerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Redirect, 
+} from "react-router-dom"; 
+
+import Home from "./Home/pages/home"
+import Carrito from "./Carrito/pages/carrito"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/Carrito" exact>
+          <Carrito />
+        </Route>
+        <Redirect to="/" />
+
+      </Switch> 
+
+    </Router>
+    
   );
 }
 
